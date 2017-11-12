d352fcc9c7fa5cd4d1d6113b602107a5aee06969
michael <michael@michaelhall.tech>
2017-11-12 05:03:29 -0500

    lets not let people load a completely broken mess

--- advroleassign/__init__.py
+++ advroleassign/__init__.py
@@ -1,9 +1,11 @@
-from .advrole import AdvRoleAssign
-import sys
+# from .advrole import AdvRoleAssign
+# import sys
 
 
 def setup(bot):
-    if sys.version_info >= (3, 6, 2):
-        bot.add_cog(AdvRoleAssign())
-    else:
-        raise RuntimeError("This package requires python version >= 3.6.2")
+    raise RuntimeError("Unfinished, this won't load")
+
+    # if sys.version_info >= (3, 6, 2):
+    #    bot.add_cog(AdvRoleAssign())
+    # else:
+    #    raise RuntimeError("This package requires python version >= 3.6.2")
