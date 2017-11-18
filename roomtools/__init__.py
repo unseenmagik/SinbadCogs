afab5c49143daf4ff37b7c7c5e8f52ee4ba8d363
michael <michael@michaelhall.tech>
2017-11-17 21:56:18 -0500

    more work

--- roomtools/__init__.py
+++ roomtools/__init__.py
@@ -9,9 +9,9 @@ def setup(bot):
         global roomtoolscmdgroup
         bot.add_cog(RoomTools(bot))
         roomtoolscmdgroup = bot.get_cog('RoomTools').roomtoolsgroup
-        if True:  # bot.get_cog('RoomTools').ar_active:
+        if bot.get_cog('RoomTools').ar_active:
             bot.add_cog(AutoRooms(bot))
-        if False:  # bot.get_cog('RoomTools').tmpc_active:
+        if bot.get_cog('RoomTools').tmpc_active:
             bot.add_cog(TempChannels(bot))
     else:
         raise RuntimeError("This package requires python version >= 3.6.2")
